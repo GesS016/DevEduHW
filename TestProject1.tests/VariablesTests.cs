4ad67d0d08cdf8d12906b84081b10a2f2df1e45f
GesS016 <gusikasadov@gmail.com>
2022-10-17 00:04:10 +0400

    negative tests

--- TestProject1.tests/VariablesTests.cs
+++ TestProject1.tests/VariablesTests.cs
@@ -7,15 +7,20 @@ namespace TestProject1.tests
     public class VariablesTests
     {
         [TestCase(2,3,19)]
-        [TestCase(4,4,0)]
+        [TestCase(4,3,-29)]
         public void SolveExampleTest(int a, int b, int expected)
         {
             int actual = Variables.SolveExample(a, b);
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(2,2)]
+        public void SolveExampleTest_AisEqualB_ShouldThrowArgumentException(int a, int b)
+        {
+            Assert.Throws<ArgumentException>(() => Variables.SolveExample(a, b));
+        }
+
         [TestCase(1,2,3,1)]
-        [TestCase(0,2,3,555)]
         [TestCase(-1,2,3,-1)]
         [TestCase(-1,3,2,1)]
         [TestCase(1,3,2,-1)]
@@ -25,11 +30,22 @@ namespace TestProject1.tests
             Assert.AreEqual(expected, actual);
         }
 
-        [TestCase(1, 1, 2, 2, "y=5x+10")]
+        [TestCase(0,1,2)]
+        public void SolveEquationTest_AisEqualZero_ShouldThrowArgumentException(int a, int b, int c)
+        {
+            Assert.Throws<ArgumentException>(() => Variables.SolveEquation(a,b,c));
+        }
+        
+        [TestCase(1, 1, 2, 2, "y=1x+0")]
         public void FindStraightLineEquationTest(int x1, int y1, int x2, int y2, string expected)
         {
             string actual = Variables.FindStraightLineEquation(x1, y1, x2, y2);
             Assert.AreEqual(expected, actual);
+        }
+
+        public void FindStraightLineEquationTest_SameDot_ShouldThrowArgumentException(int x1, int y1, int x2, int y2)
+        {
+            Assert.Throws<ArgumentException>(()=> Variables.FindStraightLineEquation(x1, y1, x2, y2));
         }
     }
 }
--- TestProject1/Variables.cs
+++ TestProject1/Variables.cs
@@ -4,18 +4,31 @@
     {
         public static int SolveExample(int a, int b) //2
         {
+           if (a == b)
+            {
+                throw new ArgumentException("a==b");
+            }
             int result = (5 * a + b * b) / (b - a);
             return result;
         }
 
         public static int SolveEquation(int a, int b, int c) //4
         {
+            if(a==0)
+            {
+                throw new ArgumentException("a==0");
+            }
             int result = (c - b) / a;
             return result;
         }
+        
 
         public static string FindStraightLineEquation(int x1, int y1, int x2, int y2) //5
         {
+            if(x1==x2 && y1 == y2)
+            {
+                throw new ArgumentException("can be any line");
+            }
             int a = (y2 - y1) / (x2 - x1);
             int b = y2 - a * x2;
             string result;
